fe369ab9ccf7c9e1289fbf9c4c37d82811acc0a8
SALSABILA SYIFA <101609243+salsabilaSyifa@users.noreply.github.com>
2022-03-17 10:04:25 +0800

    Update latihan3.kt

--- latihan3.kt
+++ latihan3.kt
@@ -1,15 +1,15 @@
-class Barang(val nama: String="nama", var harga: Double=999999.999, var jumlah: Int=99, var diskon: Double =99999.99){
+class Barang(val nama: String="nama", var harga: Double=999999.99, var jumlah: Int=99, var diskon: Int =99){
     val namaBrg: String
     val hargaBrg: Double
     val jumBrg: Int
-    val diskonBrg: Double
+    val diskonBrg: Int
 
     init {
         if (nama == "nama"){
             namaBrg="KOSONG"
             hargaBrg=0.0
             jumBrg=0
-            diskonBrg=0.0
+            diskonBrg= 0
         } else{
             namaBrg=nama.toUpperCase()
             hargaBrg= harga
@@ -21,25 +21,26 @@ class Barang(val nama: String="nama", var harga: Double=999999.999, var jumlah: 
         println("Nama : $namaBrg")
         println("Harga : $hargaBrg")
         println("Jumlah : $jumBrg")
-        println("Diskon : $diskonBrg")
+        println("Diskon : $diskonBrg %")
     }
 
     fun hitungTotal(): Double {
-        val hasil = harga*jumlah*diskon
-        return hasil
+        var hasil = harga * jumlah * diskon / 100
+        println("Harga Diskon : $hasil")
+        return harga - hasil
     }
 }
 
 fun main(array: Array<String>){
-    val barang1 = Barang("ayam",15000.00,1,0.5)
+    val barang1 = Barang("ayam",15000.00,1,10)
     barang1.tampil()
-    println("Harga Diskon: ${barang1.hitungTotal()}\n")
+    println("Harga Barang setelah Diskon : ${barang1.hitungTotal()}\n")
 
-    val barang2 = Barang("minyak",14000.00,3,0.5)
+    val barang2 = Barang("minyak",14000.00,3,5)
     barang2.tampil()
-    println("Harga Diskon: ${barang2.hitungTotal()}\n")
+    println("Harga Barang setelah Diskon : ${barang2.hitungTotal()}\n")
 
     val barang3 = Barang()
     barang3.tampil()
-    println("Harga Diskon: ${barang3.hitungTotal()}\n")
+    println("Harga Barang setelah Diskon : ${barang3.hitungTotal()}\n")
 }
